af40e3cdc3813ecced197e76bcd7ade12540fc9a
jayavibhavnk <jaya11vibhav@gmail.com>
2026-04-03 02:48:41 -0700

    tools: add WebFetch and WebSearch to exports

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -4,3 +4,5 @@ export { writeTool } from "./write.js";
 export { editTool } from "./edit.js";
 export { globTool } from "./glob.js";
 export { grepTool } from "./grep.js";
+export { webFetchTool } from "./webfetch.js";
+export { webSearchTool } from "./websearch.js";
